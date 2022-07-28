9fd8242101dd63317737620e7cdc023f1ba84549
delfinadiamante88 <108770327+delfinadiamante88@users.noreply.github.com>
2022-07-28 20:11:42 -0300

    Update astrologia.js

--- astrologia.js
+++ astrologia.js
@@ -1,1 +1,42 @@
-astrologia.js
+let signozodiacal = prompt("¿Qué signo sos?");
+
+switch (signozodiacal) {
+case "aries":
+alert("Naciste en marzo - abril");
+ break;
+case "tauro":
+alert("Naciste en abril - mayo");
+ break;
+case "géminis":
+alert("Naciste en mayo - junio");
+ break;
+case "cáncer":
+alert("Naciste en junio - julio");
+ break;
+ case "leo":
+alert("Naciste en julio - agosto");
+ break;
+ case "virgo":
+alert("Naciste en agosto - septiembre");
+ break;
+case "libra":
+alert("Naciste en septiembre - octubre");
+ break;
+case "escorpio":
+alert("Naciste en octubre - noviembre");
+ break;
+ case "sagitario":
+alert("Naciste en noviembre - diciembre");
+ break;
+case "capricornio":
+alert("Naciste en diciembre - enero");
+ break;
+ case "acuario":
+alert("Naciste en enero - febrero");
+ break;
+ case "piscis":
+alert("Naciste en febrero - marzo");
+ break;
+ default:
+alert("Escribe tu signo en minúsculas.");
+}
